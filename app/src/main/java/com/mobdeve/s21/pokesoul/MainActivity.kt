b985f7d9cbe058ef0be74936542abf802f565402
Daniella Limbag <92845499+daniellalimbag@users.noreply.github.com>
2024-10-13 23:12:33 +0800

    feat: Added fragments and logic to swap between them

--- app/src/main/java/com/mobdeve/s21/pokesoul/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s21/pokesoul/MainActivity.kt
@@ -1,24 +1,28 @@
 package com.mobdeve.s21.pokesoul
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var navbar: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navbar = findViewById(R.id.navbarBnv)
+
+        // Automatically connect BottomNavigationView with NavController
+        navbar.setupWithNavController(navController)
     }
 }
