ed0ef06ce4e8b6e90c3d939ce21ef73f36db23cc
Daniella Limbag <92845499+daniellalimbag@users.noreply.github.com>
2024-10-13 21:41:06 +0800

    refactor: Changed home back to main and added xml files for navbar

--- app/src/main/java/com/mobdeve/s21/pokesoul/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s21/pokesoul/MainActivity.kt
@@ -3,14 +3,18 @@ package com.mobdeve.s21.pokesoul
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomeActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
