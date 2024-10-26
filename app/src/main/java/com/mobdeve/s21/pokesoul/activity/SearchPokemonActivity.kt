b71bcd67526b91c6dcdd9c48ba3655a26752c07b
Hiro04526 <21hiro44@gmail.com>
2024-10-27 04:56:24 +0800

    feat: Added search pokemon activity and xml

---
+++ app/src/main/java/com/mobdeve/s21/pokesoul/activity/SearchPokemonActivity.kt
@@ -0,0 +1,12 @@
+package com.mobdeve.s21.pokesoul.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mobdeve.s21.pokesoul.R
+
+class SearchPokemonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.search_pokemon)
+    }
+}
