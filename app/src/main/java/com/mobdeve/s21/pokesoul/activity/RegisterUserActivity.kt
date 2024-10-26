501f5769071cf644efbbaf8386ec12e87f8deed9
Neonenenen <ma_julianna_reyes@dlsu.edu.ph>
2024-10-27 03:12:26 +0800

    feat: Added functionality to the Register view

--- app/src/main/java/com/mobdeve/s21/pokesoul/activity/RegisterUserActivity.kt
+++ app/src/main/java/com/mobdeve/s21/pokesoul/activity/RegisterUserActivity.kt
@@ -1,19 +1,54 @@
 package com.mobdeve.s21.pokesoul.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.mobdeve.s21.pokesoul.R
 
-class RegisterUserActivity: AppCompatActivity(){
+class RegisterUserActivity: AppCompatActivity(), View.OnClickListener{
+    lateinit var registerBtn : Button
+
+    lateinit var usernameInput : EditText
+    lateinit var  emailInput : EditText
+    lateinit var  passwordInput : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.register_user)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        registerBtn = findViewById(R.id.registerBtn)
+        usernameInput = findViewById(R.id.usernameEt)
+        emailInput = findViewById(R.id.emailEt)
+        passwordInput = findViewById(R.id.passwordEt)
 
+        registerBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.registerBtn ->{
+                val username = usernameInput.text.toString()
+                val email = emailInput.text.toString()
+                val password = passwordInput.text.toString()
+
+                if(!(username.isEmpty()) && !(email.isEmpty()) && !(password.isEmpty())){
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                else{
+                    Toast.makeText(applicationContext,"Fill in all the required information", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun EditText.isEmpty(): Boolean{
+        return this.toString().trim().isEmpty()
     }
 }
