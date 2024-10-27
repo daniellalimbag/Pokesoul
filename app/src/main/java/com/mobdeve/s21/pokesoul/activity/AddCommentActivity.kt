c2714880be8dd872d8e0b95a34618ed39e4b04ca
Neonenenen <ma_julianna_reyes@dlsu.edu.ph>
2024-10-27 20:44:53 +0800

    refactor: sending data to the view post activity

--- app/src/main/java/com/mobdeve/s21/pokesoul/activity/AddCommentActivity.kt
+++ app/src/main/java/com/mobdeve/s21/pokesoul/activity/AddCommentActivity.kt
@@ -1,25 +1,38 @@
 package com.mobdeve.s21.pokesoul.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.mobdeve.s21.pokesoul.R
 import com.mobdeve.s21.pokesoul.model.Post
+import com.mobdeve.s21.pokesoul.model.User
 
 class AddCommentActivity : AppCompatActivity(),  View.OnClickListener{
 
     private lateinit var backImgBtn : ImageButton
     private lateinit var commentBtn : Button
-    lateinit var ref_post : Post
 
+    private lateinit var contentTv : TextView
+
+    lateinit var ref_post : Post
+    lateinit var ref_user : User
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         setContentView(R.layout.add_comment)
+
+        ref_user = User("Player 1", R.drawable.player1, "I like men and women", mutableListOf(), mutableListOf())
+
+        contentTv = findViewById(R.id.contentTv)
 
         backImgBtn = findViewById(R.id.backImgBtn)
         backImgBtn.setOnClickListener(this)
@@ -28,6 +41,7 @@ class AddCommentActivity : AppCompatActivity(),  View.OnClickListener{
 
         commentBtn = findViewById(R.id.commentBtn)
         commentBtn.setOnClickListener(this)
+
 
     }
     override fun onClick(v: View?) {
@@ -38,7 +52,6 @@ class AddCommentActivity : AppCompatActivity(),  View.OnClickListener{
             }
             R.id.commentBtn -> {
 
-
                 var intent = Intent(this, ViewPostActivity::class.java)
                 intent.putExtra("post_title", ref_post.title)
                 intent.putExtra("post_creator", ref_post.creator.username)
@@ -48,6 +61,10 @@ class AddCommentActivity : AppCompatActivity(),  View.OnClickListener{
                 intent.putExtra("post_commentCount", ref_post.commentCount)
                 intent.putExtra("post_likeCount", ref_post.likeCount)
                 intent.putExtra("post_dislikeCount", ref_post.dislikeCount)
+
+                intent.putExtra("comment_user", ref_user.username)
+                intent.putExtra("comment_content", contentTv.text.toString())
+                intent.putExtra("comment_time", "1 Min ago")
                 startActivity(intent)
             }
         }
