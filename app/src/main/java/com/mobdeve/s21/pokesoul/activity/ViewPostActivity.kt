f1ba4d862bd7047a6ab85a126a0bb49c24a10aec
Neonenenen <ma_julianna_reyes@dlsu.edu.ph>
2024-10-27 14:12:03 +0800

    refactor: allowed for user to view selected post with comments

--- app/src/main/java/com/mobdeve/s21/pokesoul/activity/ViewPostActivity.kt
+++ app/src/main/java/com/mobdeve/s21/pokesoul/activity/ViewPostActivity.kt
@@ -1,9 +1,11 @@
 package com.mobdeve.s21.pokesoul.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,6 +21,16 @@ class ViewPostActivity: AppCompatActivity(), View.OnClickListener{
     private lateinit var commentsRv : RecyclerView
     private var commentList : ArrayList<Comment> = DataHelper.loadCommentData()
 
+    private lateinit var titleTextView : TextView
+    private lateinit var  creatorTextView : TextView
+    private lateinit var userSIv : ImageView
+    private lateinit var timeTv : TextView
+    private lateinit var contentTv : TextView
+    private lateinit var commentCountTv : TextView
+    private lateinit var likeCountTv : TextView
+    private lateinit var dislikeCountTv : TextView
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
@@ -27,6 +39,26 @@ class ViewPostActivity: AppCompatActivity(), View.OnClickListener{
         backIv = findViewById(R.id.backIv)
 
         commentList = DataHelper.loadCommentData()
+
+        titleTextView = findViewById(R.id.titleTv)
+        creatorTextView = findViewById(R.id.usernameTv)
+        userSIv = findViewById(R.id.userSiv)
+        timeTv = findViewById(R.id.timeTv)
+        contentTv = findViewById(R.id.contentTv)
+        commentCountTv = findViewById(R.id.commentCountTv)
+        likeCountTv = findViewById(R.id.likeCountTv)
+        dislikeCountTv = findViewById(R.id.dislikeCountTv)
+
+        val intent = intent
+
+        titleTextView.text = intent.getStringExtra("post_title")
+        creatorTextView.text = intent.getStringExtra("post_creator")
+        userSIv.setImageResource(intent.getIntExtra("post_pfp",R.drawable.death_icon))
+        timeTv.text = intent.getStringExtra("post_time")
+        contentTv.text = intent.getStringExtra("post_content")
+        commentCountTv.text = intent.getIntExtra("post_commentCount", 0).toString()
+        likeCountTv.text = intent.getIntExtra("post_likeCount", 0).toString()
+        dislikeCountTv.text = intent.getIntExtra("post_dislikeCount", 0).toString()
 
         backIv.setOnClickListener(this)
 
--- app/src/main/java/com/mobdeve/s21/pokesoul/adapter/PostAdapter.kt
+++ app/src/main/java/com/mobdeve/s21/pokesoul/adapter/PostAdapter.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.mobdeve.s21.pokesoul.R
 import com.mobdeve.s21.pokesoul.activity.ViewPostActivity
 import com.mobdeve.s21.pokesoul.model.Post
+import com.mobdeve.s21.pokesoul.model.User
 import com.mobdeve.s21.pokesoul.viewholder.PostViewHolder
 
 class PostAdapter(private val posts: List<Post>) : RecyclerView.Adapter<PostViewHolder>() {
@@ -32,6 +33,16 @@ class PostAdapter(private val posts: List<Post>) : RecyclerView.Adapter<PostView
 
         holder.postContainer.setOnClickListener{
             val intent = Intent(holder.itemView.context, ViewPostActivity::class.java)
+
+            intent.putExtra("post_title", post.title)
+            intent.putExtra("post_creator", post.creator.username)
+            intent.putExtra("post_pfp",post.creator.image)
+            intent.putExtra("post_time", post.time)
+            intent.putExtra("post_content", post.content)
+            intent.putExtra("post_commentCount", post.commentCount)
+            intent.putExtra("post_likeCount", post.likeCount)
+            intent.putExtra("post_dislikeCount", post.dislikeCount)
+
             holder.itemView.context.startActivity(intent)
         }
         holder.likeImageButton.setOnClickListener {
