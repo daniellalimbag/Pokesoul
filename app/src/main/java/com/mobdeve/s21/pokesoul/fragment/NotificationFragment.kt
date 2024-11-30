e329aa1ddffa9ec38575a3efc51f406f1de810c0
Neonenenen <ma_julianna_reyes@dlsu.edu.ph>
2024-11-30 10:36:49 +0800

    removed instances of firestore in the notificationFragment

--- app/src/main/java/com/mobdeve/s21/pokesoul/fragment/NotificationFragment.kt
+++ app/src/main/java/com/mobdeve/s21/pokesoul/fragment/NotificationFragment.kt
@@ -1,14 +1,12 @@
 package com.mobdeve.s21.pokesoul.fragment
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.firestore.FirebaseFirestore
 import com.mobdeve.s21.pokesoul.R
 import com.mobdeve.s21.pokesoul.adapter.NotificationAdapter
 import com.mobdeve.s21.pokesoul.model.Notification
@@ -17,7 +15,6 @@ class NotificationFragment : Fragment() {
     private lateinit var notificationRv: RecyclerView
     private lateinit var notificationAdapter: NotificationAdapter
     private var notifications: MutableList<Notification> = mutableListOf()
-    private val db = FirebaseFirestore.getInstance() // Firestore instance
 
 
     override fun onCreateView(
@@ -34,38 +31,6 @@ class NotificationFragment : Fragment() {
         notificationAdapter = NotificationAdapter(notifications)
         notificationRv.adapter = notificationAdapter
 
-
-        fetchNotifications()
-
         return view
     }
-
-    private fun fetchNotifications() {
-        db.collection("Notification")
-            .get() // Fetch the documents in the collection
-            .addOnSuccessListener { documents ->
-                // Clear the list in the fragment before adding new notifications
-                notifications.clear()
-
-                // Add notifications from Firestore to the list
-                for (document in documents) {
-                    // Create a Notification object from each document
-                    val notification = document.toObject(Notification::class.java)
-                    notifications.add(notification)
-                }
-
-
-                // Update the adapter with the new notifications
-                notificationAdapter.updateNotifications()
-
-                // Log the notifications to verify
-
-            }
-            .addOnFailureListener { exception ->
-                // Log any errors
-                Log.w("Notification", "Error getting documents: ", exception)
-            }
-    }
-
-
 }
