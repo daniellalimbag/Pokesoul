837195568e01bc0e6e4ccbf91956e8e946e728de
Neonenenen <ma_julianna_reyes@dlsu.edu.ph>
2024-12-05 01:03:51 +0800

    Restarting from Scratch after making the Schema

--- app/src/main/java/com/mobdeve/s21/pokesoul/database/DatabaseManager.kt
+++ app/src/main/java/com/mobdeve/s21/pokesoul/database/DatabaseManager.kt
@@ -6,27 +6,4 @@ import com.mobdeve.s21.pokesoul.model.User
 
 class DatabaseManager(context: Context) {
     private val dbHelper = MyDatabaseHelper(context)
-
-    //MANAGING USER TABLE
-    fun insertUser(user: User):Long {
-        val db = dbHelper.writableDatabase
-        val values = ContentValues().apply {
-            put(MyDatabaseHelper.USERNAME, user.username)
-            put(MyDatabaseHelper.IMAGE, user.image)
-        }
-        val result = db.insert(MyDatabaseHelper.USERS_TABLE, null, values)
-        db.close()
-        return result
-    }
-    fun deleteUser(username: String):Int {
-        val db = dbHelper.writableDatabase
-        val result = db.delete(
-            MyDatabaseHelper.USERS_TABLE,
-            "${MyDatabaseHelper.USERNAME} = ?",
-            arrayOf(username)
-        )
-        db.close()
-        return result
-    }
-
 }
--- app/src/main/java/com/mobdeve/s21/pokesoul/database/MyDatabaseHelper.kt
+++ app/src/main/java/com/mobdeve/s21/pokesoul/database/MyDatabaseHelper.kt
@@ -3,36 +3,18 @@ package com.mobdeve.s21.pokesoul.database
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import android.media.Image
 
 class MyDatabaseHelper(context: Context) : SQLiteOpenHelper(context, pokesoulDB, null, DB_ver) {
     companion object {
         private const val pokesoulDB = "my_database.db"
         private const val DB_ver = 1
-
-        //Table: User
-        const val USERS_TABLE = "users"
-        const val USER_ID = "user_id"
-        const val USERNAME = "username"
-        const val IMAGE = "image"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
 
-        // Create the users table
-        val createUsersQuery = """
-            CREATE TABLE $USERS_TABLE(
-            $USER_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-            $USERNAME TEXT NOT NULL,
-            $IMAGE INTEGER NOT NULL
-            )
-        """
-        db?.execSQL(createUsersQuery)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        db?.execSQL("DROP TABLE IF EXISTS $USERS_TABLE")
-        onCreate(db)
     }
 
 }
