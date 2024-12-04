9dc66da815d0e911277937e94342179a51846a94
Neonenenen <ma_julianna_reyes@dlsu.edu.ph>
2024-12-04 23:53:18 +0800

    User Added to the DB start up

--- app/src/main/java/com/mobdeve/s21/pokesoul/database/MyDatabaseHelper.kt
+++ app/src/main/java/com/mobdeve/s21/pokesoul/database/MyDatabaseHelper.kt
@@ -3,28 +3,35 @@ package com.mobdeve.s21.pokesoul.database
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.media.Image
 
 class MyDatabaseHelper(context: Context) : SQLiteOpenHelper(context, pokesoulDB, null, DB_ver) {
     companion object {
         private const val pokesoulDB = "my_database.db"
         private const val DB_ver = 1
-        const val TABLE_NAME = "my_table"
-        const val COLUMN_ID = "id"
-        const val COLUMN_NAME = "name"
+
+        //Table: User
+        const val USERS_TABLE = "users"
+        const val USER_ID = "user_id"
+        const val USERNAME = "username"
+        const val IMAGE = "image"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val createTableQuery = """
-            CREATE TABLE $TABLE_NAME (
-                $COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                $COLUMN_NAME TEXT
+
+        // Create the users table
+        val createUsersQuery = """
+            CREATE TABLE $USERS_TABLE(
+            $USER_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            $USERNAME TEXT NOT NULL,
+            $IMAGE INTEGER NOT NULL
             )
         """
-        db?.execSQL(createTableQuery)
+        db?.execSQL(createUsersQuery)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        db?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        db?.execSQL("DROP TABLE IF EXISTS $USERS_TABLE")
         onCreate(db)
     }
 
