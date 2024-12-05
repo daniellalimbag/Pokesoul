5d184981708c3bd29cd74545779c236217a28882
Neonenenen <ma_julianna_reyes@dlsu.edu.ph>
2024-12-05 10:48:31 +0800

    update DatabaseManager

--- app/src/main/java/com/mobdeve/s21/pokesoul/database/DatabaseManager.kt
+++ app/src/main/java/com/mobdeve/s21/pokesoul/database/DatabaseManager.kt
@@ -7,13 +7,50 @@ import android.database.sqlite.SQLiteDatabase
 class DatabaseManager(context: Context) {
     private val dbHelper = MyDatabaseHelper(context)
 
+    //Insert,Update,Delete Run
+    fun insertRunEntry(runName: String, gameTitle: String, updatedTime: String): Long? {
+        val db: SQLiteDatabase = dbHelper.writableDatabase
+        val values = ContentValues().apply {
+            put(MyDatabaseHelper.RUN_NAME, runName)
+            put(MyDatabaseHelper.RUN_GAME_TITLE, gameTitle)
+            put(MyDatabaseHelper.RUN_UPDATED_TIME, updatedTime)
+        }
+        return db.insert(MyDatabaseHelper.RUN_TABLE, null, values).also { db.close() }
+    }
+    fun updateRunEntry(id: Long, runName: String, gameTitle: String, updatedTime: String): Int {
+        val db: SQLiteDatabase = dbHelper.writableDatabase
+        val values = ContentValues().apply {
+            put(MyDatabaseHelper.RUN_NAME, runName)
+            put(MyDatabaseHelper.RUN_GAME_TITLE, gameTitle)
+            put(MyDatabaseHelper.RUN_UPDATED_TIME, updatedTime)
+        }
+        return db.update(
+            MyDatabaseHelper.RUN_TABLE,
+            values,
+            "${MyDatabaseHelper.RUN_ID} = ?",
+            arrayOf(id.toString())).also { db.close() }
+    }
+    fun deleteRunEntry(id: Long): Int {
+        val db: SQLiteDatabase = dbHelper.writableDatabase
+        val values = ContentValues().apply {
+            put(MyDatabaseHelper.RUN_ID, id)
+        }
+        return db.delete(
+            MyDatabaseHelper.RUN_TABLE,
+            "${MyDatabaseHelper.RUN_ID} = ?",
+            arrayOf(id.toString())
+        ).also { db.close() }
+    }
+
+
     //Insert,Update,Delete OwnedPokemon
-    fun insertOwnedPokemonEntry(nickname: String, caughtLocation: String, savedLocation: String, runId: Long): Long? {
+    fun insertOwnedPokemonEntry(nickname: String, caughtLocation: String, savedLocation: String, runId: Long): Long {
         val db: SQLiteDatabase = dbHelper.writableDatabase
             val values = ContentValues().apply {
                 put(MyDatabaseHelper.OWNED_POKEMON_NICKNAME, nickname)
                 put(MyDatabaseHelper.OWNED_POKEMON_CAUGHT_LOCATION, caughtLocation)
                 put(MyDatabaseHelper.OWNED_POKEMON_SAVED_LOCATION, savedLocation)
+                put(MyDatabaseHelper.RUN_ID, runId)
             }
             return  db.insert(MyDatabaseHelper.OWNED_POKEMON_TABLE, null, values).also { db.close() }
     }
